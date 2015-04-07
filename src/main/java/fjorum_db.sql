b02aad768458245fe6dfec6f8d98d74e63753020
Daniel Gronau <daniel.gronau@skillcert.de>
2015-04-07 10:29:26 +0200

    improved db script

--- src/main/java/fjorum_db.sql
+++ src/main/java/fjorum_db.sql
@@ -4,19 +4,7 @@ CREATE TABLE category
   cat_name   VARCHAR(256)       NOT NULL,
   sort_order INT
 );
-CREATE TABLE category_id_seq
-(
-  sequence_name VARCHAR NOT NULL,
-  last_value    BIGINT  NOT NULL,
-  start_value   BIGINT  NOT NULL,
-  increment_by  BIGINT  NOT NULL,
-  max_value     BIGINT  NOT NULL,
-  min_value     BIGINT  NOT NULL,
-  cache_value   BIGINT  NOT NULL,
-  log_cnt       BIGINT  NOT NULL,
-  is_cycled     BOOL    NOT NULL,
-  is_called     BOOL    NOT NULL
-);
+
 CREATE TABLE reply
 (
   id            SERIAL PRIMARY KEY NOT NULL,
@@ -26,19 +14,7 @@ CREATE TABLE reply
   modified      TIMESTAMP          NOT NULL,
   reply_content VARCHAR            NOT NULL
 );
-CREATE TABLE reply_id_seq
-(
-  sequence_name VARCHAR NOT NULL,
-  last_value    BIGINT  NOT NULL,
-  start_value   BIGINT  NOT NULL,
-  increment_by  BIGINT  NOT NULL,
-  max_value     BIGINT  NOT NULL,
-  min_value     BIGINT  NOT NULL,
-  cache_value   BIGINT  NOT NULL,
-  log_cnt       BIGINT  NOT NULL,
-  is_cycled     BOOL    NOT NULL,
-  is_called     BOOL    NOT NULL
-);
+
 CREATE TABLE topic
 (
   id          SERIAL PRIMARY KEY NOT NULL,
@@ -49,19 +25,7 @@ CREATE TABLE topic
   open        BOOL               NOT NULL,
   sticky      BOOL               NOT NULL
 );
-CREATE TABLE topic_id_seq
-(
-  sequence_name VARCHAR NOT NULL,
-  last_value    BIGINT  NOT NULL,
-  start_value   BIGINT  NOT NULL,
-  increment_by  BIGINT  NOT NULL,
-  max_value     BIGINT  NOT NULL,
-  min_value     BIGINT  NOT NULL,
-  cache_value   BIGINT  NOT NULL,
-  log_cnt       BIGINT  NOT NULL,
-  is_cycled     BOOL    NOT NULL,
-  is_called     BOOL    NOT NULL
-);
+
 CREATE TABLE users
 (
   id               SERIAL PRIMARY KEY NOT NULL,
@@ -74,19 +38,7 @@ CREATE TABLE users
   moderator        BOOL DEFAULT FALSE NOT NULL,
   administrator    BOOL DEFAULT FALSE NOT NULL
 );
-CREATE TABLE users_id_seq
-(
-  sequence_name VARCHAR NOT NULL,
-  last_value    BIGINT  NOT NULL,
-  start_value   BIGINT  NOT NULL,
-  increment_by  BIGINT  NOT NULL,
-  max_value     BIGINT  NOT NULL,
-  min_value     BIGINT  NOT NULL,
-  cache_value   BIGINT  NOT NULL,
-  log_cnt       BIGINT  NOT NULL,
-  is_cycled     BOOL    NOT NULL,
-  is_called     BOOL    NOT NULL
-);
+
 ALTER TABLE reply ADD FOREIGN KEY (topic_id) REFERENCES topic (id) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE reply ADD FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE topic ADD FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE ON UPDATE CASCADE;
@@ -97,3 +49,5 @@ CREATE UNIQUE INDEX users_user_name_key ON users (user_name);
 INSERT INTO users VALUES
   (0, 'admin', '$2a$10$fXBiVP42zuQM1p4.1gZ0L.eWpHbtEUIr3BKE3Ssa5xircp/IJzfVS', '', '', 'admin@fjorum.com', TRUE, TRUE,
    TRUE);
+
+CREATE ROLE fjorum WITH LOGIN SUPERUSER PASSWORD 'fjorum';
